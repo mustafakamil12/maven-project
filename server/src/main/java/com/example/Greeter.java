6c000a8a9d4df8da252224e3d6fbcf6b084553d1
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Pro.local>
2018-03-27 17:45:49 -0500

    last fixing

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,11 +12,9 @@ public class Greeter {
 
   }
 
-  /*
-   *
+  /**
    Some Test Comments and do some changes here...
-   *
-   */
+  **/
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
